f91632af8e627482d595bb9d5e32beebf78311a4
hariharan <hariharan7477@gmail.com>
2023-04-12 12:52:36 +0530

    Include fundamental starter projects.Closes #1.

---
+++ eshopping-cart/src/main/java/com/shoppingcart/eshoppingcart/EshoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.eshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshoppingCartApplication.class, args);
+	}
+
+}
---
+++ eshopping-cart/src/test/java/com/shoppingcart/eshoppingcart/EshoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.eshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
